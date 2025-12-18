ce2d30edbef8ddba96cd67d68fd4769d98910c51
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2025-12-18 09:01:41 +0800

    Create config.js

---
+++ sinlaudemo/config.js
@@ -0,0 +1,7 @@
+// config.js
+// 這裡是全站共用的設定檔
+
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbyY_jDRwTOJLR4OJeDX0Ikissf8-vQcJ_CTzs25EsL1pnazJv_-94LzoVESXL6gqQWW/exec';
+
+// 如果未來有其他共用設定，也可以加在這裡
+// const HOSPITAL_NAME = '新樓醫院';
