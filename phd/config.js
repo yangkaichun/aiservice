efda929353fb4f267f833c22bdfd3267c7cb781a
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2025-12-20 12:40:23 +0800

    Update config.js

--- phd/config.js
+++ phd/config.js
@@ -1,2 +1,2 @@
 // Google Apps Script 部署網址 (Web App URL)
-const API_URL = "https://script.google.com/macros/s/AKfycbxnPRql5z2ugJ_BxvInnkagmzuEyd5xLr9Ju_a2tBOaALGRmjH5CHRi4xzFSjxpKaux2Q/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyMA6qWQhQBEocdtHZU4ALhSMfzFtGNkd-x1KrwD9cd-B90wH8UGfZAmb1j_eLdGIQVGQ/exec";
