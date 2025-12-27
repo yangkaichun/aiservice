e367a433808eb011719483dbc8d7c2c68abbe9b2
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2025-12-27 17:01:05 +0800

    Update config.js

--- phd/config.js
+++ phd/config.js
@@ -1,4 +1,4 @@
 
 const config = {
-    API_URL: "https://script.google.com/macros/s/AKfycbxkl9qZ82YscqViPxYduknGToHuXC1ZumczvBavaPoA-GzlQNca_0xNTLSsVpCjeYTfzQ/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbx7VDgX1ZfV09dy0l6MJDc-pdiLnvtVe7eCKqx5n6svfxdAqlO76DJ51bziW6bto0kY5Q/exec"
 };
