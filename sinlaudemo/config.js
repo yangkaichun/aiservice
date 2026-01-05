44bfa863203e2a343f26358d28d6fda5429270d9
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-01-05 15:55:22 +0800

    Update config.js

--- sinlaudemo/config.js
+++ sinlaudemo/config.js
@@ -6,7 +6,7 @@
  */
 
 // Google Apps Script 部署網址 (後端 API)
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwqrUyaLceVU7G0r1INgwYtvQH8TVSaHiCOiDICzWiOuyDusVGP6xALVPA59DctdtvK/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzhmiHZbnNxl4wCof-rToNHskzGgrxw79UvqGt2x1QXfMdA6fcGQQ6bo0Tco3ealDXB/exec';
 
 // Google OAuth 2.0 用戶端 ID (用於管理員登入驗證)
 const GOOGLE_CLIENT_ID = '1087860144140-upfitccfmtj5ibsg8tk9o0gu2efgt819.apps.googleusercontent.com';
