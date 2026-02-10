73192ec8a5b5222a796f5c7f5c893df4de753ef6
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-02-10 17:00:36 +0800

    Update config.js

--- pancadai/CRM/config.js
+++ pancadai/CRM/config.js
@@ -3,5 +3,5 @@ const CONFIG = {
   GAS_API_URL: "https://script.google.com/macros/s/AKfycbztsdR__FONltm1unK6v-oUEzrdbZEJgYlSt3txq_HpM8S-ZuHvc99sWqsxK-OQcEY0iQ/exec",
   
   // GCP 的 Client ID (請填入第一階段申請的 ID)
-  GOOGLE_CLIENT_ID: "1yblDhfRoDT-Bb0MqdSW_Wun7-QxDOQ5qtKp7K4kkz30442690120777-f3skgcs0a38bfdieu7co92343l1c3kmd.apps.googleusercontent.com"
+  GOOGLE_CLIENT_ID: "442690120777-f3skgcs0a38bfdieu7co92343l1c3kmd.apps.googleusercontent.com"
 };
