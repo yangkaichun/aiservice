c1be6ae0c338cf743906b4a3033b108cf138fe0d
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-02-10 14:03:51 +0800

    Create config.js

---
+++ pancadai/CRM/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  // 後端 GAS 的網址 (請填入剛剛部署拿到的 URL)
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbztsdR__FONltm1unK6v-oUEzrdbZEJgYlSt3txq_HpM8S-ZuHvc99sWqsxK-OQcEY0iQ/exec",
+  
+  // GCP 的 Client ID (請填入第一階段申請的 ID)
+  GOOGLE_CLIENT_ID: "1yblDhfRoDT-Bb0MqdSW_Wun7-QxDOQ5qtKp7K4kkz30442690120777-f3skgcs0a38bfdieu7co92343l1c3kmd.apps.googleusercontent.com"
+};
