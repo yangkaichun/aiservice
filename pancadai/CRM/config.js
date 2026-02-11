b01fc4f5250d572d749a0033b212097ce82337c4
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-02-11 09:43:02 +0800

    Update config.js

--- pancadai/CRM/config.js
+++ pancadai/CRM/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // 後端 GAS 的網址 (請填入剛剛部署拿到的 URL)
-  GAS_API_URL: "https://script.google.com/macros/s/AKfycbztsdR__FONltm1unK6v-oUEzrdbZEJgYlSt3txq_HpM8S-ZuHvc99sWqsxK-OQcEY0iQ/exec",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbx-cpQiPVYjXKfBY5qbY-bG4xPUc9ouidVvsT-4r17wsaXR6J_qoon3BK0rnOLWKgv-kA/exec",
   
   // GCP 的 Client ID (請填入第一階段申請的 ID)
   GOOGLE_CLIENT_ID: "442690120777-f3skgcs0a38bfdieu7co92343l1c3kmd.apps.googleusercontent.com"
