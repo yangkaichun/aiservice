842bebdeeb13819e41ed3bfacc355f14965a3b9c
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-02-25 05:54:02 +0800

    Update config.js

--- pancadai/CRM/config.js
+++ pancadai/CRM/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // 剛才複製的 GAS Web App URL
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyUbxNUigO27sMsvFBZWMMsyUvcWomvowBq3rBOyC-cae0qYy8wmy3sZwCHF8HQxERVSQ/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxBEEG0sb3QjY__8FejAXnxdKqTabaYJvYc-17y4OuSbr_LpaMTF8CGMYlYOHpZEIe90Q/exec",
     // 剛才複製的 GCP Client ID
     GOOGLE_CLIENT_ID: "469731622214-uiqtuprq4293g3j09nfq5c43uvldadfj.apps.googleusercontent.com",
     ADMIN_EMAIL: "kaichun.yang@gmail.com"
