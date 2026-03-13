8f6d391631d503887759c5866c197330a924efad
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-03-13 12:43:29 +0800

    Create config.js

---
+++ pancadai/tripfee/config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+    // 請將下方的網址替換成您剛剛在 GAS 部署產生的「網頁應用程式網址 (Web App URL)」
+    GAS_API_URL: "https://script.google.com/macros/s/AKfycbxiwhKXa0ACfOVgTQ-5YLaMpKQ9BTuKm7lMrWnhIzzCZDegMe_Fnt9TLqnm_ohhaVGQPg/exec" 
+};
