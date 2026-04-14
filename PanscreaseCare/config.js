bb3848b436b26729f5d2439205de189c90ce63a0
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-04-14 09:39:26 +0800

    Create config.js

---
+++ PanscreaseCare/config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+    // 替換為您的 Apps Script 網頁應用程式網址
+    GAS_URL: "https://script.google.com/macros/s/AKfycbyJ-V11wjsGGlWx-Yxc6qE-mqfDX41qnUSh71vODLGslkpTlcKbT9Bc9hzJ1haymkXo9A/exec"
+};
