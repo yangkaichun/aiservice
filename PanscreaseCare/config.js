2a4d4e93a015ce1ef447f5dd6d359584ea6eaf33
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-04-23 12:16:56 +0800

    Update config.js

--- PanscreaseCare/config.js
+++ PanscreaseCare/config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
     // 替換為您的 Apps Script 網頁應用程式網址
-    GAS_URL: "https://script.google.com/macros/s/AKfycbxiMsbMgv3cMFPFxsB8gvdSf9XwsbWZrbsZX0BvzwAg1C27kWyJQT_MF88jAYnGIMUp-w/exec"
+    GAS_URL: "https://script.google.com/macros/s/AKfycbxcXEjccpouh2czuuedbJEXCUOHCOjLUwPsERaTe9RdZvU5j80d28tEwfZ7QzM6RoQLeQ/exec"
 };
