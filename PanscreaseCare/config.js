40386bbb4f5b6fc52245987c7ffd4fa5a9c78bfe
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-04-27 09:02:12 +0800

    Update config.js

--- PanscreaseCare/config.js
+++ PanscreaseCare/config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
     // 您最新發布的 Google Apps Script (API) 網址
-    GAS_URL: "https://script.google.com/macros/s/AKfycby_yEVQ0W5Bs6ojcvSQC7a3e1N-f6UDlA_KuylfcAd9KbAfs6cjgl-mLlaQJb9EviB41g/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycby89oHDnwbbhfKNQsrgUKyrvVsfAY0f0z4jAbKCgd92LxvTOrKgDFOLRTU9l1AA28zu9g/exec",
     
     // Google OAuth 登入專用的 Client ID
     CLIENT_ID: "15631583768-m6bf6st6cpdtrqudue4lmblf6l96sghg.apps.googleusercontent.com"
