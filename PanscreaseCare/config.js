4de69673ba083640190c42ad12a8f042cb2aa9d0
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-04-27 08:28:13 +0800

    Update config.js

--- PanscreaseCare/config.js
+++ PanscreaseCare/config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
     // 這是您最新發布、擁有完整 Google Drive 讀寫權限的 API 網址
-    GAS_URL: "https://script.google.com/macros/s/AKfycbzF1eZFGbHWUibrK_RPiFENPWXSyWHLHzJTTANaBxKbIR57l_ncOBKFtTSWXAS1MT48wA/exec"
+    GAS_URL: "https://script.google.com/macros/s/AKfycbzQ1KBd1oJ_uYtRq_C0nJ496ukNw_GQY2fxwpJcIo3rHxvEhAdzuDIRdBgQVSU4nSnxZA/exec"
 };
