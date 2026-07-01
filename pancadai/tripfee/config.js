9d083bcc2a8f6ee3d4e641e5f61de0062dc2f5e4
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-07-01 17:05:58 +0800

    Update config.js

--- pancadai/tripfee/config.js
+++ pancadai/tripfee/config.js
@@ -2,7 +2,7 @@
 // config.js
 const CONFIG = {
     // 替換為您的 GAS Web App URL (需重新部署後的最新網址)
-    GAS_API_URL: "https://script.google.com/macros/s/AKfycbzLFkXn22sH-Gg6C-PRcJ1gicJ6OFNhJNamhfZYDgSMy1QCu6gxV7NRLhYSyfCmSyaKMg/exec", 
+    GAS_API_URL: "https://script.google.com/macros/s/AKfycbzUMSM9BIZy86PKSAKVxb1eX5vSDw1i__UA4lUTP2lH8sr8_t-rUxG8qIcNJFjuCwY8/exec", 
     
     // 您申請的 Google Client ID
     GOOGLE_CLIENT_ID: "470505739418-tdi7edodl65q0hpr9272kd4uof3d7l9v.apps.googleusercontent.com" 
