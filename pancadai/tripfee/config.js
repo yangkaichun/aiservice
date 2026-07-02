4079d96ea8e43916d4abaf6dd87d35871b2ec11b
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-07-02 09:54:49 +0800

    Update config.js

--- pancadai/tripfee/config.js
+++ pancadai/tripfee/config.js
@@ -5,5 +5,5 @@ const CONFIG = {
     GAS_API_URL: "https://script.google.com/macros/s/AKfycbxlOkdDWdq5suZ3_RAA1uA8P28-YNUsblhDDIG6w26R1z8gXWKmQ-_dm3m791U9LZEVRg/exec",
 
     // 您申請的 Google Client ID
-    GOOGLE_CLIENT_ID: "470505739418-tdi7edodl65q0hpr9272kd4uof3d7l9v.apps.googleusercontent.com",
+    GOOGLE_CLIENT_ID: "https://script.google.com/macros/s/AKfycbygPMUQLl23DE9L6T1ksTNJyK6VHNgIXZQmX0v2L3U6ghv1QkNa77tWiOuyg5FGaWaKpw/exec",
 };
