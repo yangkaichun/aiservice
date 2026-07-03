04b227f3f5009757ad4eb8a3fe48981c3d747c73
yangkaichun <yangkaichun@users.noreply.github.com>
2026-07-03 04:14:25 +0000

    deploy: bedd6f63741ee70c750c75f8ce1b00bb458b901d

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // config.js
 const CONFIG = {
     // 替換為您的 GAS Web App URL (需重新部署後的最新網址)
-    GAS_API_URL: "https://script.google.com/macros/s/AKfycbwvmuYyR8AgMr_v1XAifEO7GNJQkuHpAdFoYHXhR6G0f1WV7_SipjefcvtQfkw5BbboIA/exec",
+    GAS_API_URL: "https://script.google.com/macros/s/AKfycbzWz-rNmG2wlxr1xklZQv5qr7RKDveG_TC4p4fsSUyNXdcfPdKCWeJcqhGiBRC__xw/exec",
 
     // 您申請的 Google Client ID
     GOOGLE_CLIENT_ID: "470505739418-tdi7edodl65q0hpr9272kd4uof3d7l9v.apps.googleusercontent.com",
