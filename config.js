a540eea0e33f44b56bbc1e4f6e6fcb9873e33122
yangkaichun <yangkaichun@users.noreply.github.com>
2026-07-03 06:33:20 +0000

    deploy: 87663f00b50ada495eef1e65427fd1c9c49c9a42

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 // config.js
 const CONFIG = {
-    GAS_API_URL: "https://script.google.com/macros/s/AKfycbwsdLJvQenHuIh9CXGyImp7hRKOAvCs5eg4d3DUNevSgijsbakW3bXVBbrg2v1Q14Xozg/exec",
+    GAS_API_URL: "https://script.google.com/macros/s/AKfycbywtoS-1k0OgaWUyWbNi98VSolC3OIjs_xBgw_fCj6OLASauOZ6ryd12CEJjGwxWJUVQQ/exec",
     GOOGLE_CLIENT_ID: "470505739418-tdi7edodl65q0hpr9272kd4uof3d7l9v.apps.googleusercontent.com",
 };
