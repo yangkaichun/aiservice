81f21b37230ae080da961d0b2673e73ba6ce29a6
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-07-03 13:02:55 +0800

    Update config.js

--- pancadai/tripfee2/config.js
+++ pancadai/tripfee2/config.js
@@ -1,6 +1,6 @@
 
 // config.js
 const CONFIG = {
-    GAS_API_URL: "https://script.google.com/macros/s/AKfycbyJv8Q-aGMjPA0nicyKXix3K5eftH5wCusmN-LzyMNOwXdmQVUDskSmaac28zFt8E5XyA/exec",
+    GAS_API_URL: "https://script.google.com/macros/s/AKfycbwsdLJvQenHuIh9CXGyImp7hRKOAvCs5eg4d3DUNevSgijsbakW3bXVBbrg2v1Q14Xozg/exec",
     GOOGLE_CLIENT_ID: "470505739418-tdi7edodl65q0hpr9272kd4uof3d7l9v.apps.googleusercontent.com",
 };
