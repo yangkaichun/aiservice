dc6fde509d62ca5d6503c8fb119bd0689fab7136
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2026-07-03 09:23:21 +0800

    Delete pancadai/tripfee directory

--- pancadai/tripfee/config.js
+++
@@ -1,9 +0,0 @@
-
-// config.js
-const CONFIG = {
-    // 替換為您的 GAS Web App URL (需重新部署後的最新網址)
-    GAS_API_URL: "https://script.google.com/macros/s/AKfycbxWgi74-NTGG7ENqN74QUI044I-c_VaHxigfRGvnI-pNN9G_MDc9irpX-T-xbf9OQypkA/exec",
-
-    // 您申請的 Google Client ID
-    GOOGLE_CLIENT_ID: "470505739418-tdi7edodl65q0hpr9272kd4uof3d7l9v.apps.googleusercontent.com",
-};
