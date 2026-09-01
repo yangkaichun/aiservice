b1a78c2e3d40b783a69cfd87583fcc136fb6ddd9
yangkaichun <yangkaichun@users.noreply.github.com>
2026-09-01 08:45:43 +0000

    deploy: c2160426947b606c10dd1c75d865a3f357711061

--- PancreasCare/config.js
+++ PancreasCare/config.js
@@ -1,7 +1,7 @@
 // config.js
 const CONFIG = {
     // 您最新發布的 Google Apps Script (API) 網址
-    GAS_URL: "https://script.google.com/macros/s/AKfycbzHCKA74Kns23TohIHIfhfEjBK9y32j9XwMWGFGhTKJAL6HNOBVfTH4xmYaXKoTyR0kdw/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbzK82yNsEmhSsa5UvU88--XJ6E0XmQwNWYc4c4V8Q9YfQfbvYN5NRXOtBd1-3UIznlIQA/exec",
     
     // Google OAuth 登入專用的 Client ID
     CLIENT_ID: "15631583768-m6bf6st6cpdtrqudue4lmblf6l96sghg.apps.googleusercontent.com"
